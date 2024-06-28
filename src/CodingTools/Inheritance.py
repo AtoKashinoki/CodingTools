0bfc416b231c109cfcdde849c1efbbdef0420e07
AtoKashinoki <atokashinoki@gmail.com>
2024-06-28 10:03:14 +0900

    Update DataClass

--- src/CodingTools/Inheritance.py
+++ src/CodingTools/Inheritance.py
@@ -13,6 +13,28 @@ from abc import (ABC, abstractmethod)
 """ DataClassInheritanceClass """
 
 
+def protect_member(instance, key: str) -> str:
+    """
+        Generate protect member.
+    :param instance: class instance.
+    :param key: variable name.
+    :return: protect member key.
+    """
+    return "_" + instance.__class__.__name__ + key
+
+
+def get_variable_name(instance, key) -> str:
+    """
+    Check protect member and Return variable name.
+    :param instance: class instance.
+    :param key: variable name.
+    :return: variable name key.
+    """
+    if key[:2] == "__":
+        return protect_member(instance, key)
+    return key
+
+
 class DataClass(ABC):
     """
         DataClass inheritance class.
@@ -24,6 +46,21 @@ class DataClass(ABC):
     # constant variables
     __PRINT_DATAS: str = Constant("{}{}")
     __DESCRIPTORS: set[type] = Constant({DataType, Constant})
+
+    def set_values(self, **kwargs):
+        """
+            Initialize datas.
+        :param kwargs: key -> value name : value to assign.
+        """
+        [
+            setattr(
+                self,
+                get_variable_name(self, key),
+                value
+            )
+            for key, value in kwargs.items()
+        ]
+        return
 
     def __repr__(self):
         return self.__PRINT_DATAS.format(
--- tests/Inheritance.py
+++ tests/Inheritance.py
@@ -9,6 +9,9 @@ from CodingTools.Inheritance import DataClass
 class DataClassTest(DataClass):
     test1: str = DataType(str)
     test2: str = Constant("test2")
+    __test3: str = DataType(str)
+    @property
+    def test3(self) -> str: return self.__test3
     ...
 
 
@@ -17,6 +20,10 @@ class Test:
 
     def __init__(self):
         self.test_datas.test1 = "test1"
+        self.test_datas.set_values(
+            __test3="test3",
+            test4="test4",
+        )
         # self.test_datas.test1 = 1
         # self.test_datas.test2 = ""
         return
