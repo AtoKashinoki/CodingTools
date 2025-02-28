c952362323b6a8a1018a9004ccfecbfd66a4cbab
AtoKashinoki <atokashinoki@gmail.com>
2025-02-28 23:00:01 +0900

    Add rmtree tool

--- src/CodingTools/os.py
+++ src/CodingTools/os.py
@@ -9,9 +9,14 @@ This file contains the os-relate tools for used for developing in python.
 
 
 import os
+import shutil
+
+from typing import Callable
 
 
 """ os processes """
+
+""" directory """
 
 
 def mkdir(_path: str) -> bool:
@@ -21,4 +26,29 @@ def mkdir(_path: str) -> bool:
     """
     if os.path.exists(_path): return False
     os.mkdir(_path)
+    return True
+
+
+def caveat_rmtree() -> bool:
+    """
+        Caveat from rmtree
+    :return bool: True if the user was approved.
+    """
+    if not input("Are you sure you want to delete it? [Y|n]: ") in ("Y", ):
+        return False
+    return True
+
+
+def rmtree(
+        _path: str,
+        caveat_process: Callable[[], bool] = caveat_rmtree
+) -> bool:
+    """
+        Remove a directory and its contents
+
+    :return bool: True if the directory and its contents were removed.
+    """
+    if not caveat_process():
+        return False
+    shutil.rmtree(_path)
     return True
