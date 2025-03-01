9478f2f8f9d12138f9117f2b82c77c227bdff370
AtoKashinoki <atokashinoki@gmail.com>
2025-03-01 16:41:26 +0900

    Add mkrote tool

--- src/CodingTools/os.py
+++ src/CodingTools/os.py
@@ -36,6 +36,22 @@ caveat_rmtree = ConsoleCaveat.create(
 )
 
 
+def mk_rote(
+        _start_path: str,
+        _dir_names: str,
+) -> bool:
+    """
+    Make directory rote
+    :return: True if the directory rote was created.
+    """
+    path = _start_path
+    for dir_name in _dir_names:
+        path = os.path.join(path, dir_name)
+        mkdir(path)
+        continue
+    return True
+
+
 def rmtree(
         _path: str,
         caveat_process: Callable[[dict[str, Any]], bool] = caveat_rmtree
